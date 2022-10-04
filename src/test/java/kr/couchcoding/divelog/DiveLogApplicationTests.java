dd6e160f6f0b3b11fdde13933fec239156768fae
godongs <asd236678@gmail.com>
2022-10-04 16:12:30 +0900

    init

---
+++ src/test/java/kr/couchcoding/divelog/DiveLogApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.couchcoding.divelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiveLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
