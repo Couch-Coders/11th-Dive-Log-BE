0e5dc2bdea5f3754d63fcd5028262fb556b194fa
godongs <asd236678@gmail.com>
2022-10-04 22:32:10 +0900

    add logs

--- src/main/java/kr/couchcoding/divelog/DiveLogApplication.java
+++ src/main/java/kr/couchcoding/divelog/DiveLogApplication.java
@@ -1,12 +1,17 @@
 package kr.couchcoding.divelog;
 
+import org.hibernate.annotations.common.util.impl.Log_.logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class DiveLogApplication {
 
 	public static void main(String[] args) {
+		log.info("database url : " + System.getenv("JDBC_DATABASE_URL"));
 		SpringApplication.run(DiveLogApplication.class, args);
 	}
 
