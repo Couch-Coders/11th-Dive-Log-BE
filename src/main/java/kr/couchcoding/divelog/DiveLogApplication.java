5dc0d825e11a758e4980e7fa8825f2bde011894c
godongs <asd236678@gmail.com>
2022-10-04 22:40:09 +0900

    remove import

--- src/main/java/kr/couchcoding/divelog/DiveLogApplication.java
+++ src/main/java/kr/couchcoding/divelog/DiveLogApplication.java
@@ -1,6 +1,5 @@
 package kr.couchcoding.divelog;
 
-import org.hibernate.annotations.common.util.impl.Log_.logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
