74faedea8dffc27746cbe2cd81273b10390d4abe
godongs <asd236678@gmail.com>
2022-10-05 17:39:27 +0900

    add local db

--- create-db.sh
+++ create-db.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 docker run -d --name dive-log-db -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=dive-log -p 5431:5432 postgres:12
+
+export JDBC_DATABASE_URL=jdbc:postgresql://localhost:5431/dive-log?user=postgres&password=postgres&sslmode=disable
