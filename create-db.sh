1e455ed5f8f1f6852ea97776ab9279438636d014
godongs <asd236678@gmail.com>
2022-10-07 21:30:18 +0900

    Add Firebase config

--- create-db.sh
+++ create-db.sh
@@ -2,4 +2,4 @@
 
 docker run -d --name dive-log-db -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=dive-log -p 5431:5432 postgres:12
 
-export JDBC_DATABASE_URL=jdbc:postgresql://localhost:5431/dive-log?user=postgres&password=postgres&sslmode=disable
+export JDBC_DATABASE_URL="jdbc:postgresql://localhost:5431/dive-log?user=postgres&password=postgres&sslmode=disable"
---
+++ src/main/java/kr/couchcoding/divelog/config/FirebaseConfig.java
@@ -0,0 +1,40 @@
+package kr.couchcoding.divelog.config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@Slf4j
+public class FirebaseConfig {
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        log.info("Initializing Firebase.");
+        FileInputStream serviceAccount = new FileInputStream("./firebaseAdminKey.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp app = FirebaseApp.initializeApp(options);
+        log.info("FirebaseApp initialized: {}" , app.getName());
+
+        return app;
+    }
+
+    // FirebaseAuth를 초기화 한다
+    @Bean
+    public FirebaseAuth getFirebaseAuth() throws IOException {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance(firebaseApp());
+        return firebaseAuth;
+    }
+}
