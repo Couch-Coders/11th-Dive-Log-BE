441b599dba69167029e602fb626ca40b20abdac5
godongs <asd236678@gmail.com>
2022-10-08 17:49:00 +0900

    change \" to * & fix bugs

--- scripts/run-qoddi.sh
+++ scripts/run-qoddi.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-echo ${FIREBASE_ADMIN_KEY} >> ./firebaseAdminKey.json 
-sed -i 's/\"/"/g' firebaseAdminKey.json
+echo ${FIREBASE_ADMIN_KEY} >> env.json 
+cat env.json
+sed 's/*/"/g' firebaseAdminKey.json
+cat firebaseAdminKey.json
 java -Dserver.port=$PORT $JAVA_OPTS -Dspring.profiles.active=prod -jar ./build/libs/dive-log-0.0.1-SNAPSHOT.jar
