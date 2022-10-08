5fd4b0eca97c4013fc8bfeba76df65b77e42542e
godongs <asd236678@gmail.com>
2022-10-08 18:06:05 +0900

    remove logs

--- scripts/run-qoddi.sh
+++ scripts/run-qoddi.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 echo ${FIREBASE_ADMIN_KEY} >> env.json 
-cat env.json
 sed 's/*/"/g' env.json >> firebaseAdminKey.json
-cat firebaseAdminKey.json
 java -Dserver.port=$PORT $JAVA_OPTS -Dspring.profiles.active=prod -jar ./build/libs/dive-log-0.0.1-SNAPSHOT.jar
