ccd1072e09fd4adba21151445caf487eea5036a0
godongs <asd236678@gmail.com>
2022-10-08 18:33:36 +0900

    add exception for revoke token

---
+++ src/main/java/kr/couchcoding/divelog/exception/RevokeTokenException.java
@@ -0,0 +1,7 @@
+package kr.couchcoding.divelog.exception;
+
+public class RevokeTokenException extends Exception{
+    public RevokeTokenException(String message) {
+        super(message);
+    }
+}
