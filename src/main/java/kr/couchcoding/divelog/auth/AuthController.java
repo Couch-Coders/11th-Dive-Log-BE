7637218805624e367c5a2c4b18c2ea2733788696
godongs <asd236678@gmail.com>
2022-10-08 18:33:40 +0900

    add logout

--- src/main/java/kr/couchcoding/divelog/auth/AuthController.java
+++ src/main/java/kr/couchcoding/divelog/auth/AuthController.java
@@ -3,6 +3,8 @@ package kr.couchcoding.divelog.auth;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import kr.couchcoding.divelog.auth.dto.AuthInfo;
 import kr.couchcoding.divelog.auth.service.AuthService;
 import kr.couchcoding.divelog.exception.InvalidAuthTokenException;
+import kr.couchcoding.divelog.exception.RevokeTokenException;
 import kr.couchcoding.divelog.user.User;
 import kr.couchcoding.divelog.user.UserDto;
 import lombok.RequiredArgsConstructor;
@@ -35,6 +38,14 @@ public class AuthController {
         return ResponseEntity.ok().header("idToken", cookie.toString()).body(new UserDto(user));
     }
 
+    @DeleteMapping("/logout")
+    public ResponseEntity<Void> logout(Authentication authentication) throws RevokeTokenException {
+        User user = (User) authentication.getPrincipal();
+        authService.revokeToken(user);
+
+        return ResponseEntity.ok().header("idToken", removeCookie("idToken").toString()).build();
+    }
+
     private String getTokenString(String bearerToken) throws InvalidAuthTokenException {
         String token = bearerToken.substring(7);
         if(token == null || token.isEmpty()){
@@ -48,6 +59,15 @@ public class AuthController {
             .httpOnly(true)
             .secure(true)
             .path("/")
+            .build();
+    }
+
+    private ResponseCookie removeCookie(String name) {
+        return ResponseCookie.from(name, "")
+            .httpOnly(true)
+            .secure(true)
+            .path("/")
+            .maxAge(0)
             .build();
     }
     
