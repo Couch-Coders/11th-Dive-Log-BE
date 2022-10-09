f49dd615e870576ed78058dfe6bca95694c87cf5
godongs <asd236678@gmail.com>
2022-10-09 23:40:04 +0900

    add log controller

---
+++ src/main/java/kr/couchcoding/divelog/log/LogController.java
@@ -0,0 +1,10 @@
+package kr.couchcoding.divelog.log;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/logs")
+public class LogController {
+    
+}
