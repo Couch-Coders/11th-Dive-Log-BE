f51a0f8788b4297de17cd8a7b6841b1e6183437d
godongs <asd236678@gmail.com>
2022-10-09 23:39:52 +0900

    add log entity

---
+++ src/main/java/kr/couchcoding/divelog/log/Log.java
@@ -0,0 +1,72 @@
+package kr.couchcoding.divelog.log;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+import kr.couchcoding.divelog.user.User;
+import lombok.Builder;
+import lombok.Getter;
+
+
+@Entity
+@Table(name = "log", schema = "public")
+@Getter
+public class Log {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    private LocalDateTime date;
+    @Enumerated(EnumType.STRING)
+    private DiveType diveType;
+    private LocalDateTime enterTime;
+    private LocalDateTime leaveTime;
+    private int sight;
+    private int maxDepth;
+    private int temperature;
+    private int maxOxygen;
+    private int minOxygen;
+    private String location;
+    private String content;
+    @ElementCollection
+    private List<String> images = new ArrayList<>();
+    private double longitude;
+    private double latitude;
+
+    
+    @Builder
+    public Log(User user, LocalDateTime date, DiveType diveType, LocalDateTime enterTime, LocalDateTime leaveTime,
+            int sight, int maxDepth, int temperature, int maxOxygen, int minOxygen, String location, String content,
+            List<String> images, double longitude, double latitude) {
+        this.user = user;
+        this.date = date;
+        this.diveType = diveType;
+        this.enterTime = enterTime;
+        this.leaveTime = leaveTime;
+        this.sight = sight;
+        this.maxDepth = maxDepth;
+        this.temperature = temperature;
+        this.maxOxygen = maxOxygen;
+        this.minOxygen = minOxygen;
+        this.location = location;
+        this.content = content;
+        this.images = images;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    } 
+}
