5e9c340a7140cd3c225518596d726f665c07fd9f
godongs <asd236678@gmail.com>
2022-10-11 11:04:16 +0900

    remove page

--- src/main/java/kr/couchcoding/divelog/log/dao/LogRepository.java
+++ src/main/java/kr/couchcoding/divelog/log/dao/LogRepository.java
@@ -1,12 +1,9 @@
 package kr.couchcoding.divelog.log.dao;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import kr.couchcoding.divelog.log.Log;
-import kr.couchcoding.divelog.user.User;
 
 @Repository
 public interface LogRepository extends JpaRepository<Log, Long>, LogRepositoryCustom {
