3b7606443043704ba6ecb973cc966e65182d5a25
godongs <asd236678@gmail.com>
2022-10-11 09:37:11 +0900

    remove errors

--- src/main/java/kr/couchcoding/divelog/log/dao/LogRepositoryCustom.java
+++ src/main/java/kr/couchcoding/divelog/log/dao/LogRepositoryCustom.java
@@ -1,10 +1,11 @@
 package kr.couchcoding.divelog.log.dao;
 
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.domain.Page;
 
 import kr.couchcoding.divelog.log.Log;
 import kr.couchcoding.divelog.user.User;
 
-public class LogRepositoryCustom {
-    Page<Log> findAllBySearchOption(User user, ,  Pageable pageable);
+public interface LogRepositoryCustom {
+    Page<Log> findAllBySearchOption(User user, Pageable pageable);
 }
